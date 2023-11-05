bd8a0f7031a3071798ae9370cac8afb09afc20aa
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2023-11-05 14:33:35 -0300

    broken and outadated packages removed, fixed and upgraded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
