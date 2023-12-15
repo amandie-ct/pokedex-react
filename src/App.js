baded8cf7a8c09fc3a55e8c418bbe9d7e9dce493
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2023-12-15 13:49:32 -0300

    changing from switch to router from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import Wallpaper from './Wallpaper';
 import Pokedex from './Pokedex';
 import SinglePokemon from './SinglePokemon';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Wallpaper/>
-        <Switch>
-          <Route exact path="/">
-            <Pokedex/>
-          </Route>
-          <Route path="/pokemon/:id">
-            <SinglePokemon/>
-          </Route>
-        </Switch>
-    </Router>
+        <Router>
+          <Route exact path="/" element={ <Pokedex/> } />
+          <Route path="/pokemon/:id" element={ <SinglePokemon/> } />
+        </Router>
+    </BrowserRouter>
   );
 }
 
